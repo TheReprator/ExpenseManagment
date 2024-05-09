6423bc781fd2a897ad7c19e6d344a8e9673cea00
Reprator <mr.vikram004@gmail.com>
2024-05-09 12:08:12 +0530

    IOS App added with xcode, scheme and launched icon added, integration with main app pending

---
+++ iosApp/Accountbook/Accountbook/AccountbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountbookApp.swift
+//  Accountbook
+//
+//  Created by Vikram Singh on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AccountbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
