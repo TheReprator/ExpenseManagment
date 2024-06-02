35341457aa533141485a7155ac4437827dd9ab7a
Vikram Singh <mr.vikram004@gmail.com>
2024-06-03 00:16:16 +0530

    Readme updated, tivi reference removed

--- server/lib/base/src/main/kotlin/dev/reprator/base/action/AppLogger.kt
+++ server/lib/base/src/main/kotlin/dev/reprator/base/action/AppLogger.kt
@@ -1,6 +1,3 @@
-// Copyright 2018, Google LLC, Christopher Banes and the Tivi project contributors
-// SPDX-License-Identifier: Apache-2.0
-
 package dev.reprator.base.action
 
 interface AppLogger {
